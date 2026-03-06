3bcfdc8352a884f804326f39d5570e63421deab4
Rohit165555 <111436038+Rohit165555@users.noreply.github.com>
2026-03-06 20:31:43 +0530

    Add files via upload

---
+++ proj/pollproject/pollapp/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+
+admin.site.site_header = "Pollproject Admin"
+admin.site.site_title = "Pollproject Admin Area"
+admin.site.index_title = "Welcome to the Pollproject Admin Area"
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {
+            'fields': ['pub_date'],
+            'classes': ['collapse']
+        }),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
